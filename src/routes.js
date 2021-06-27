a0e2b157fbea45bdb08eaf625fec9f5d7df7a24f
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-06-27 21:39:50 +0100

    feed profile page
    use <%= profile["vacation-per-year"]  %>

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,19 @@ const routes = express.Router()
 
 const views = __dirname + '/views/'
 
+const profile = {
+    name: 'Claudio Lins',
+    avatar: 'https://avatars.githubusercontent.com/u/69011104?v=4',
+    "monthly-budget": 1500,
+    "hours-per-day": 10,
+    "days-per-week": 5,
+    "vacation-per-year": 4,
+
+}
+
 routes.get('/', (req, res) => res.render(views + 'index'))
 routes.get('/job', (req, res) => res.render(views + 'job'))
 routes.get('/job/edit', (req, res) => res.render(views + 'job-edit'))
-routes.get('/profile', (req, res) => res.render(views + 'profile'))
+routes.get('/profile', (req, res) => res.render(views + 'profile', {profile}))
 
 module.exports = routes
