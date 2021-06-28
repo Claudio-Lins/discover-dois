3e987e150232c6cd0649f2ed6beca5baaac51313
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-06-28 20:19:21 +0100

    habilitar o uso do req.body
    server.use(express.urlencoded({ extended: true }))

--- src/server.js
+++ src/server.js
@@ -6,5 +6,8 @@ server.set('view engine', 'ejs')
 
 server.use(express.static('public'))
 
+//Usar o req.body
+server.use(express.urlencoded({ extended: true }))
+
 server.use(routes)
 server.listen(3000, () => console.log('Rodando'))
